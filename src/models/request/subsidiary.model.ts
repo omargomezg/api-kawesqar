41ba8c048a08c6acdfd8a8fda0e56861f601fedc
Omar Gómez <omar.gomez@bnamericas.com>
2019-06-13 17:55:08 -0400

    add service subsidary

--- src/models/request/subsidiary.model.ts
+++ src/models/request/subsidiary.model.ts
@@ -1,3 +1,4 @@
+
 export class ISubsidiaryPostModel {
     private idSucursal: number;
     private rutSucursal: string;
@@ -12,19 +13,30 @@ export class ISubsidiaryPostModel {
     private registroContado: boolean;
     private numInicialRegContado: number;
 
-    constructor(private idSucursal: number,
-        private rutSucursal: string,
-        private nombre: string,
-        private direccion: string,
-        private codigo: number,
-        private telefono: string,
-        private rutRepLegal: string,
-        private nombreRepLegal: string,
-        private fax: string,
-        private giro: string,
-        private registroContado: boolean,
-        private numInicialRegContado: number){
-            this.rutSucursal = rutSucursal;
-            
+    constructor(idSucursal: number,
+                rutSucursal: string,
+                nombre: string,
+                direccion: string,
+                codigo: number,
+                telefono: string,
+                rutRepLegal: string,
+                nombreRepLegal: string,
+                fax: string,
+                giro: string,
+                registroContado: boolean,
+                numInicialRegContado: number) {
+        this.idSucursal = idSucursal;
+        this.rutSucursal = rutSucursal;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.codigo = codigo;
+        this.telefono = telefono;
+        this.rutRepLegal = rutRepLegal;
+        this.nombreRepLegal = nombreRepLegal;
+        this.fax = fax;
+        this.giro = giro;
+        this.registroContado = registroContado;
+        this.numInicialRegContado = numInicialRegContado;
     }
+
 }
