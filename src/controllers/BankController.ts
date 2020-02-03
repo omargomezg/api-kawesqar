7b7582708c77248462ab5140bca9e35330fbc49b
omargomezg <omar.fdo.gomez@gmail.com>
2020-02-02 22:46:58 -0300

    add bank list

--- src/controllers/BankController.ts
+++ src/controllers/BankController.ts
@@ -1,13 +1,19 @@
-import { Body, JsonController, Put } from "routing-controllers";
+import { Body, Get, JsonController, Put } from "routing-controllers";
+import { createQueryBuilder } from "typeorm";
 import { Bank } from "../entities/Bank";
 import { BankRepository } from "../repository/BankRepository";
 
 @JsonController("/bank")
 export class BankController {
-    private bankRepo = new BankRepository();
+  private bankRepo = new BankRepository();
 
-    @Put("/")
-    public create(@Body() bank: Bank) {
-        return this.bankRepo.createBank(bank);
-    }
+  @Get("/")
+  public async getAll() {
+    return await createQueryBuilder("Bank").getMany();
+  }
+
+  @Put("/")
+  public create(@Body() bank: Bank) {
+    return this.bankRepo.createBank(bank);
+  }
 }
