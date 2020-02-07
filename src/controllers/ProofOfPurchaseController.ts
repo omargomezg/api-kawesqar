dd857f710cff02e0104f736a19c4c0f2150ae247
omargomezg <omar.fdo.gomez@gmail.com>
2020-02-06 23:28:15 -0300

    partial commit

--- src/controllers/ProofOfPurchaseController.ts
+++ src/controllers/ProofOfPurchaseController.ts
@@ -1,18 +1,16 @@
-import {Delete, Get, HeaderParam, JsonController, Param} from "routing-controllers";
+import {Body, Delete, Get, HeaderParam, JsonController, NotFoundError, Param, Put} from "routing-controllers";
 import {createQueryBuilder} from "typeorm";
 import {ProofOfPurchase} from "../entities/ProofOfPurchase";
 import {ProofOfPurchaseDetail} from "../entities/ProofOfPurchaseDetail";
 import {ProofOfPurchaseService} from "../service/proof-of-purchase.service";
 import {CommonController} from "./CommonController";
 import {Client} from "../entities/Client";
+import {ProofOfPurchaseRepository} from "../repository/ProofOfPurchaseRepository";
 
 @JsonController("/proof-of-purchase")
 export class HeaderController extends CommonController {
     @Get("/:id")
-    public async getById(
-        @Param("id") id: number,
-        @HeaderParam("UsrKey") usrKey: string
-    ) {
+    public async getById(@Param("id") id: number, @HeaderParam("UsrKey") usrKey: string) {
         const user = await this.getUser(usrKey);
         let result: ProofOfPurchase = new ProofOfPurchase();
         if (id === -1) {
@@ -35,6 +33,16 @@ export class HeaderController extends CommonController {
             }
         }
         return result;
+    }
+
+    @Put("/:id")
+    public async save(@Param("id") id: number, @HeaderParam("UsrKey") usrKey: string, @Body() proofOfPurchase: ProofOfPurchase) {
+        const proofRepository = new ProofOfPurchaseRepository();
+        const find = await ProofOfPurchase.find({id: id});
+        if (find.length > 0) {
+            throw new NotFoundError(`Data was not found.`);
+        }
+        ProofOfPurchase
     }
 
     @Delete("/:id")
